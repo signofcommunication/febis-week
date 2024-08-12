fc67d759eeef53e024edd6eab7a3fcdaa6962579
signofcommunication <jhvonsky@gmail.com>
2024-08-12 14:00:16 +0700

    Feat: Update font size homepage

--- src/app/Home/Hero.tsx
+++ src/app/Home/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
       id="febis-week-banner"
     >
       <Container className="text-white text-center">
-        <h1 className="text-6xl font-bold uppercase ">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold uppercase">
           <Balancer>Febis Week Presents</Balancer>
         </h1>
-        <h1 className="text-4xl font-bold uppercase">
-          <Balancer>CREATIVE ECONOMY FOR SUSTAINABLE DEVELOPMENT</Balancer>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold uppercase">
+          <Balancer>CREATIVE ECONOMY FOR SUSTAINABLE DEVELOPMENT</Balancer>
         </h1>
       </Container>
     </Section>
