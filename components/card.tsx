484cc3066007b5212bd584f2a33dee5749457544
signofcommunication <jhvonsky@gmail.com>
2024-08-19 11:45:53 +0700

    Feat: Update Crah Production on link registration

--- components/card.tsx
+++ components/card.tsx
@@ -27,6 +27,7 @@ export const CardWithForm: React.FC<CardContent> = ({
           <span className="w-56 h-48 rounded bg-yellow-500 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
           <a
             href={link}
+            target="_blank"
             className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-black p-2"
           >
             Daftar Sekarang
